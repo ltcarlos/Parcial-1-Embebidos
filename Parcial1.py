faf80a2888c78f2b27d14ee4182ad55b5217bad6
ltcarlos <carlos.rey123@hotmail.com>
2025-05-03 20:46:29 -0500

    Corrijo parcial

--- Parcial1.py
+++ Parcial1.py
@@ -16,20 +16,21 @@ def Bezier(n,PO,P1,P2,P3):
         dir_P3=uctypes.addressof(P3)
         LRN=[]
         for j in range(len(PO)):
-            C1=t3*mem32(dir_PO+4*j)
-            C2=3*t2*mem32(dir_P1+4*j)*t
-            C3=3*t*t*t1*mem32(dir_P2+4*j)
-            C4=t*t*t*mem32(dir_P3+4*j)
+            C1=t3*mem32[dir_PO+4*j]
+            C2=3*t2*mem32[dir_P1+4*j]*t
+            C3=3*t*t*t1*mem32[dir_P2+4*j]
+            C4=t*t*t*mem32[dir_P3+4*j]
             CT=C1+C2+C3+C4
             LRN.append(CT)
         LR.append(LRN)
     return(LR)
 
 n=3
-PO=[0,0]
-P1=[1,0]
-P2=[1,1]
-P3=[0,1]
+PO = array.array('i', [0, 0])
+P1 = array.array('i', [1000, 0])
+P2 = array.array('i', [1000, 1000])
+P3 = array.array('i', [0, 1000])
 print(Bezier(n,PO,P1,P2,P3))
         
     
+
